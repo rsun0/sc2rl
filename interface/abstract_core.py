6361b3a2b8e7fbedacbcb6f1f245540e53a1fd48
Ray Sun <raysun2@illinois.edu>
2019-07-03 23:09:58 -0500

    Formatted abstract_core

--- interface/abstract_core.py
+++ interface/abstract_core.py
@@ -24,52 +24,47 @@ class Experiment:
         """
         Trains the agent(s) using the custom environment
         """
-        total_frame_count = 0
-        averages = [0 for i in range(len(self.agents))]
+        total_steps = 0
         evaluation_rewards = [deque(maxlen=100) for i in range(len(self.agents))]
         averages = [[] for i in range(len(self.agents))]
         
-
         for e in range(self.run_settings.num_episodes):
             # Initialize episode
-            env_states, rewards, dones, metainfo = self.custom_env.reset()
-            done = dones[0]
+            env_states, rewards, done, metainfo = self.custom_env.reset()
             
             # Initialize scores to starting reward (probably 0)
             scores = rewards
             step = 0
             
             while not done:
-               
-                states = [self.agents[a].state_space_converter(env_states[a]) for a in range(len(self.agents))]
-            
-                total_frame_count += 1
+                states = [self.agents[a].state_space_converter(env_states[a])
+                    for a in range(len(self.agents))]
 
                 # Train agents
-                if total_frame_count % self.run_settings.train_every == 0:
+                if total_steps > 0 and total_steps % self.run_settings.train_every == 0:
                     for agent in self.agents:
                         agent.train(self.run_settings)
                         
                 # Save agent model
-                if total_frame_count % self.run_settings.save_every == 0:
+                if total_steps > 0 and total_steps % self.run_settings.save_every == 0:
                     for agent in self.agents:
                         agent.save()
 
                 # Get actions
-                actions = [self.agents[a].sample(states[a]) for a in range(len(self.agents))]
-                env_actions = [self.agents[a].action_space_converter(actions[a]) for a in range(len(self.agents))]
+                actions = [self.agents[a].sample(states[a])
+                    for a in range(len(self.agents))]
+                env_actions = [self.agents[a].action_space_converter(actions[a])
+                    for a in range(len(self.agents))]
                 # Take environment step
-                env_states, rewards, dones, metainfo = self.custom_env.step(env_actions)
-                
+                env_states, rewards, done, metainfo = self.custom_env.step(env_actions)
+                step += 1
+                total_steps += 1
                 
                 # Update scores
                 scores = [scores[a] + rewards[a] for a in range(len(self.agents))]
                 # Push to agent Memories
                 for a in range(len(self.agents)):
-                    self.agents[a].push_memory(states[a], actions[a], rewards[a], dones[a])
-                
-                done = dones[0]
-                step += 1
+                    self.agents[a].push_memory(states[a], actions[a], rewards[a], done)
                 
                 if done:
                     curr_averages = []
@@ -82,12 +77,14 @@ class Experiment:
                         scores = scores[0]
                         curr_averages = curr_averages[0]
                     recent_mean = np.mean(evaluation_rewards)
-                    print("Game %d ended after %d steps. Game score: %s. Averages: %s" % (e+1, step, scores, curr_averages))
+                    print("Game {} ended after {} steps. Game score: {}. Averages: {}"
+                        .format(e+1, step, scores, curr_averages))
                     
-            if (e % 50 == 0 and e != 0):
+            if e > 0 and e % self.run_settings.graph_every == 0:
                 self.plot_results(averages)
-                
-    def plot_results(self, averages):
+
+    @staticmethod            
+    def plot_results(averages):
         plt.figure(1)
         plt.clf()
         plt.suptitle("Training results")
@@ -125,16 +122,19 @@ class CustomEnvironment():
 
 
 class RunSettings:
-    def __init__(self, num_episodes, num_epochs, batch_size, train_every, save_every):
+    def __init__(self, num_episodes, num_epochs, batch_size, train_every,
+            save_every, graph_every):
         """
         :param num_episodes: The total number of episodes to play
         :param num_epochs: The number of update iterations for each experience set
         :param batch_size: The number of experiences to process at once
         :param train_every: Update the networks every X frames
         :param save_every: Save the model every X frames
+        :param graph_every: Graph the evaluation metrics every X episodes
         """
         self.num_episodes = num_episodes
         self.num_epochs = num_epochs
         self.batch_size = batch_size
         self.train_every = train_every
         self.save_every = save_every
+        self.graph_every = graph_every
