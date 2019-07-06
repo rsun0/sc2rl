484e9a426971469f8573f488e5e8ec28520dceac
Ray Sun <raysun2@illinois.edu>
2019-07-06 14:14:31 -0700

    Cleaned up modified_state_space

--- experiments/agent_ppo/modified_state_space.py
+++ experiments/agent_ppo/modified_state_space.py
@@ -1,25 +1,19 @@
-
 """
-This file contains modified_state_space(obs), a function for generating a modified,
-simplified state space for the pysc2 learning environment.
-Author : Michael McGuire
-Date : 02/14/2018
+Contains state_modifier.modified_state_space(obs), which transforms the state
+data given by MinigameEnvironment
+Author : Michael McGuire, Ray Sun
+Date : 07/06/2019
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+from scipy.spatial.distance import cdist
+import numpy as np
 from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features, units
 from pysc2.env import environment
 
 from config import *
-from scipy.spatial.distance import cdist
 
-
-import numpy as np
 def zero_one_norm(array):
     arr_max = np.max(array)
     arr_min = np.min(array)
@@ -29,9 +23,9 @@ def zero_one_norm(array):
     return (array - arr_min) / denom
 
 class state_modifier():        
-
-    def graph_conv_modifier(obs):
     
+    @staticmethod
+    def graph_conv_modifier(obs):
         _PLAYER_FRIENDLY = 1
         _PLAYER_HOSTILE = 4
         
@@ -62,16 +56,15 @@ class state_modifier():
 
         return np.array([np.expand_dims(G,0), np.expand_dims(X,0), avail_actions, info])
 
-
+    @staticmethod
     def modified_state_space(obs):
-        
         '''
             IN: obs
             OUT: preprocessed values for
                 screen (84 x 84 x 17)
                 minimap (84 x 84 x 7)    
         '''
-        
+
         scr = obs.observation.feature_screen
         mmap = obs.observation.feature_minimap
         player = obs.observation.player
@@ -82,11 +75,10 @@ class state_modifier():
         
         avail_actions = state_modifier.preprocess_actions(obs.observation.available_actions, DeepMind2017Config)
 
-        
         return np.array([proc_scr, proc_mmap, player, avail_actions])
         
+    @staticmethod
     def preprocess_units(units):
-    
         (n,d) = units.shape
         graph_n = GraphConvConfigMinigames.graph_n
         output = np.zeros((GraphConvConfigMinigames.graph_n, GraphConvConfigMinigames.unit_vec_width))
@@ -111,7 +103,7 @@ class state_modifier():
                     output[:n,j+idx] = np.log(units[:,j]+1)
         return output
                 
-        
+    @staticmethod
     def to_graph(units):
         (n, d) = units.shape
         
@@ -137,7 +129,6 @@ class state_modifier():
         ep_idx = enemy[np.argpartition(enemy_player, k2-1, axis=1)[:,:k2]]
         eo_idx = friendly[np.argpartition(enemy_opponent, k1-1, axis=1)[:,:k1]]
         
-        
         output[np.expand_dims(friendly,1), fp_idx] = 1.0
         output[np.expand_dims(friendly,1), fo_idx] = 1.0
         output[np.expand_dims(enemy, 1), ep_idx] = 1.0
@@ -145,36 +136,32 @@ class state_modifier():
         
         return output
         
+    @staticmethod
     def preprocess_actions(actions, my_config):
         available = np.zeros((my_config.action_space))
         for i in actions:
             if i in my_config.env_agent_action_mapper.keys():
                 ind = my_config.env_agent_action_mapper[i]
                 available[ind] = 1
-
-                
         return available
-    """
-    def preprocess_actions(actions):
-        available = np.zeros((1,DeepMind2017Config.action_space))
-        for i in actions:
-            if i in DeepMind2017Config.env_agent_action_mapper.keys():
-                available[0,DeepMind2017Config.env_agent_action_mapper[i]] = 1
-        return available
-    """    
-        
-    def preprocess_featuremap(x, features=None, out_shape=None):
     
+    # @staticmethod
+    # def preprocess_actions(actions):
+    #     available = np.zeros((1,DeepMind2017Config.action_space))
+    #     for i in actions:
+    #         if i in DeepMind2017Config.env_agent_action_mapper.keys():
+    #             available[0,DeepMind2017Config.env_agent_action_mapper[i]] = 1
+    #     return available
+    
+    @staticmethod
+    def preprocess_featuremap(x, features=None, out_shape=None):
         '''
-        
         if features is None, return numpy array
         else return coo format indices, values
-        
         '''
     
         if (type(features) == type(None)):
             return np.log(x.clip(0) + 1).reshape((1, len(x), 1, 1))
-    
     
         (_,w,h) = x.shape
         
@@ -185,9 +172,7 @@ class state_modifier():
         #preprocessed_features = np.zeros(out_shape)
         features_depth = 0
         
-        for i in range(len(features)):
-        
-            
+        for i in range(len(features)):    
             name, scale, featuretype = (features[i].name, features[i].scale, features[i].type)
             
             vals = np.array(x[name])
@@ -217,32 +202,28 @@ class state_modifier():
             
             features_depth += dim
             
-            
-            """
-            if (featuretype == CATEGORICAL):
-                dim = scale
-                if (dim == 2):
-                    dim=1
-                    #addition = np.expand_dims(x[name], 0)
-                    preprocessed_features[features_depth:features_depth+dim] = x[name]
-                else:
-                    addition = preprocessed_features[features_depth:features_depth+dim]
+            # if (featuretype == CATEGORICAL):
+            #     dim = scale
+            #     if (dim == 2):
+            #         dim=1
+            #         #addition = np.expand_dims(x[name], 0)
+            #         preprocessed_features[features_depth:features_depth+dim] = x[name]
+            #     else:
+            #         addition = preprocessed_features[features_depth:features_depth+dim]
                     
-                    vals = np.array(x[name])
-                    nonzero_indices = vals.nonzero()
-                    val_list = vals[nonzero_indices]
-                    indices = list(nonzero_indices)
-                    indices.insert(0, list(val_list))
-                    addition[tuple(indices)] = 1
+            #         vals = np.array(x[name])
+            #         nonzero_indices = vals.nonzero()
+            #         val_list = vals[nonzero_indices]
+            #         indices = list(nonzero_indices)
+            #         indices.insert(0, list(val_list))
+            #         addition[tuple(indices)] = 1
                     
-            else:
-                dim = 1
-                preprocessed_features[features_depth:features_depth+dim]
-                addition = np.expand_dims(np.log(x[name].clip(0) + 1), 0)
+            # else:
+            #     dim = 1
+            #     preprocessed_features[features_depth:features_depth+dim]
+            #     addition = np.expand_dims(np.log(x[name].clip(0) + 1), 0)
                 
-            #preprocessed_features[features_depth:features_depth+dim] = addition
-            features_depth += dim
-            """
-            
+            # #preprocessed_features[features_depth:features_depth+dim] = addition
+            # features_depth += dim
                 
         return [indices, values.astype(np.float32)]
