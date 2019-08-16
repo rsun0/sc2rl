Changed relus to leaky, added nan debugging logic, fixed spatial args computation

experiments/DeepMind2018/RRLNetwork.py
@@ -43,7 +43,7 @@ class RRLModel(BaseNetwork):
         self.inputs2d_MLP = nn.Sequential(
             nn.Linear(self.player_features + net_config["action_embedding_size"],
                         128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(128, net_config["inputs2d_size"])
         )
 
@@ -67,21 +67,21 @@ class RRLModel(BaseNetwork):
             Squeeze(),
             nn.Linear(net_config['relational_heads'] * net_config['relational_features'],
                         512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, 512),
-            nn.ReLU()
+            nn.LeakyReLU()
         )
 
 
         self.value_MLP = nn.Sequential(
             nn.Linear(512 + net_config["inputs2d_size"], 512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, 1)
         )
 
         self.action_MLP = nn.Sequential(
             nn.Linear(512 + net_config["inputs2d_size"], 512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, env_config["action_space"])
         )
 
@@ -182,6 +182,7 @@ class RRLModel(BaseNetwork):
         value = self.value_MLP(shared_features)
         action_logits_in = self.action_MLP(shared_features)
         action_logits_in = action_logits_in.masked_fill(1-avail_actions, float('-inf'))
+        #print("actions: ", torch.max(action_logits_in, dim=-1))
         action_logits = F.softmax(action_logits_in)
         #action_logits = action_logits / torch.sum(action_logits, axis=-1)
         choice = None
@@ -199,6 +200,7 @@ class RRLModel(BaseNetwork):
         arg_logit_inputs = self.arg_MLP(shared_conditioned)
         arg_logit_inputs = arg_logit_inputs.reshape((N, self.arg_depth, self.arg_size))
         arg_logits = self.generate_arg_logits(arg_logit_inputs)
+        #print("args: ", torch.sum(arg_logit_inputs, dim=-1))
 
         args = None
         if (choosing):
@@ -210,6 +212,7 @@ class RRLModel(BaseNetwork):
         spatial_input = torch.cat([relational_spatial, embedded_action], dim=1)
         spatial_logits_in = self.spatial_upsampler(spatial_input)
         spatial_logits = self.generate_spatial_logits(spatial_logits_in)
+        #print("spatial: ", torch.sum(spatial_logits_in, dim=(-1,-2)))
 
 
         spatial = None

interface/base_agent/Network.py
@@ -114,7 +114,7 @@ class BaseNetwork(nn.Module, Model):
                                         inputs2d=inputs2d[i:i+batch_size]
                                         )
             t6 = time.time()
-            
+
             irrelevant_mask = relevant_frames[i:i+batch_size] == 0
             t7 = time.time()
             #print(irrelevant_mask)
@@ -163,6 +163,7 @@ class BaseNetwork(nn.Module, Model):
 
     def sample_func(self, probs):
         probs = probs.detach().cpu().numpy().astype(np.float64)
+        assert (np.sum(probs) > 0)
         probs = probs / np.sum(probs)
         return np.argmax(np.random.multinomial(1, probs))
 

interface/base_agent/base_agent.py
@@ -30,7 +30,7 @@ import numpy as np
 import time
 import math
 import matplotlib.pyplot as plt
-
+import sys
 import copy
 
 
@@ -94,6 +94,7 @@ class BaseAgent(Agent):
                 if (math.isnan(d_pol) or math.isnan(d_vf) or math.isnan(d_ent)):
                     print("Canceling training -- NaN's encountered")
                     print("Reloading model from previous save")
+                    sys.exit(0)
                     self.load()
                     self.update_target_net()
                     return
@@ -387,6 +388,12 @@ class BaseAgent(Agent):
         self.optimizer.zero_grad()
         total_loss.backward()
         #self.process_gradients(self.model)
+        print("actions: ", torch.max(gathered_actions).item(), torch.min(gathered_actions).item())
+        print("args: ", torch.max(gathered_args).item(), torch.min(gathered_args).item())
+        print("spatial args: ", torch.max(gathered_spatial_args).item(), torch.min(gathered_spatial_args).item())
+        print("ratio: ", torch.max(ratio).item(), torch.min(ratio).item())
+        print()
+        self.process_network(self.model)
         clip_grad_norm_(self.model.parameters(), 100.0)
         self.optimizer.step()
         t7 = time.time()
@@ -394,7 +401,6 @@ class BaseAgent(Agent):
         vf_loss = value_loss.detach().item()
         ent_total = ent.detach().item()
         #print("%f %f %f %f %f %f, total: %f" % (t2-t1, t3-t2, t4-t3, t5-t4, t6-t5, t7-t6, t7-t1))
-
         return pol_loss, vf_loss, -ent_total
 
 
@@ -464,12 +470,23 @@ class BaseAgent(Agent):
 
     def process_gradients(self, network, clip=1.0):
         grad_sum = 0
-        max_param = -np.inf
-        min_param = np.inf
         for param in network.parameters():
-            print(torch.max(param), torch.min(param))
             if param.grad is None:
                 continue
             grad_sum += torch.sum(param.grad.data ** 2)
-        print(max_param, min_param)
         print(grad_sum)
+
+    def process_network(self, network):
+        nan_sum = 0
+        grad_nan_sum = 0
+        nan_denom = 0.
+        grad_nan_denom = 0.
+        for param in network.parameters():
+            nan_sum += torch.sum(torch.isnan(param))
+            grad_nan_sum += torch.sum(torch.isnan(param.grad.data))
+            nan_denom += param.numel()
+            grad_nan_denom += param.numel()
+        print(nan_sum / nan_denom, grad_nan_sum / grad_nan_denom)
+        if (nan_sum > 0 or grad_nan_sum > 0):
+            self.save()
+            sys.exit(0)

interface/base_agent/net_utils.py
@@ -199,7 +199,7 @@ class Downsampler(nn.Module):
                 kernel_size=(4,4),
                 stride=2,
                 padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(),
 
             ResnetBlock(net_config['down_conv_features'],
                             net_config['relational_depth']),
@@ -213,7 +213,7 @@ class Downsampler(nn.Module):
                 kernel_size=(4,4),
                 stride=2,
                 padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(),
 
             ResnetBlock(2*net_config['down_conv_features'],
                             net_config['relational_depth']),
@@ -227,7 +227,7 @@ class Downsampler(nn.Module):
                 kernel_size=(4,4),
                 stride=2,
                 padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(),
 
             ResnetBlock(4*net_config['down_conv_features'],
                             net_config['relational_depth']),
@@ -252,7 +252,7 @@ class SpatialUpsampler(nn.Module):
                 stride=2,
                 padding=1
             ),
-            nn.ReLU()
+            nn.LeakyReLU()
         )
 
         self.tconv2 = nn.Sequential(
@@ -263,7 +263,7 @@ class SpatialUpsampler(nn.Module):
                 stride=2,
                 padding=1
             ),
-            nn.ReLU()
+            nn.LeakyReLU()
         )
 
         self.conv_out = nn.Sequential(
@@ -272,8 +272,7 @@ class SpatialUpsampler(nn.Module):
                                         kernel_size=1,
                                         stride=1,
                                         padding=0
-            ),
-            nn.ReLU()
+            )
         )
 
     def forward(self, x):