95bbf0be0394a682e76d5d8163c350a5d9a1d1b5
Michael <mcguiremichael1998@gmail.com>
2019-08-18 09:45:05 -0500

    Fixed minor bugs

--- experiments/DeepMind2018/run.py
+++ experiments/DeepMind2018/run.py
@@ -18,7 +18,7 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 def main():
 
     map_name = "DefeatZerglingsAndBanelings"
-    render = True
+    render = False
     step_mul = 8
 
 
@@ -98,8 +98,8 @@ def main():
         "eps_denom": 1e-8,
         "c1": 0.1,
         "c2": 0.2,
-        "c3": 0.3,
-        "c4": 0.3,
+        "c3": 1.0,
+        "c4": 1.0,
         "clip_param": 0.1,
         "min_clip_param": 0.01,
         "clip_decay": 10000,
@@ -107,7 +107,7 @@ def main():
     }
 
     agent = BaseAgent(model, agent_settings, memory, train_settings)
-    agent.load()
+    #agent.load()
     experiment = Experiment([agent], env, run_settings)
 
     experiment.train()
