28a87e5e593a076c92efb702345fc18d987e1368
Ray Sun <raysun2@illinois.edu>
2019-10-25 13:07:56 -0500

    Tested MCTS agent against random agent

--- pommerman/run.py
+++ pommerman/run.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.insert(0, "../interface/")
 
+import pommerman.agents
+
 from abstract_core import Experiment, RunSettings
 from custom_env import PommermanEnvironment
 from simple_agent import SimpleAgent
 from noop_agent import NoopAgent
+from random_agent import RandomAgent
 from mcts_agent import MCTSAgent
 
 if __name__ == '__main__':
@@ -21,8 +24,8 @@ if __name__ == '__main__':
         graph_file='pommerman_results.png'
     )
 
-    agent1 = SimpleAgent()
-    agent2 = MCTSAgent()
+    agent1 = RandomAgent()
+    agent2 = MCTSAgent(opponent=pommerman.agents.RandomAgent())
     agent2.load()
 
     experiment = Experiment([agent1, agent2], env, run_settings)
