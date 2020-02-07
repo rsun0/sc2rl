5b928ed6e508133c2893daad3496aae92c997405
Ray Sun <raysun2@illinois.edu>
2020-02-07 11:35:30 -0600

    Made progress on build_depot

--- experiments/mcts_ac/action_interface.py
+++ experiments/mcts_ac/action_interface.py
@@ -18,7 +18,9 @@ class BuildMarinesActuator:
         self.reset()
 
     def reset(self):
-        pass
+        self.in_progress = None
+        self.depot_placed = False
+        self.barracks_placed = False
 
     def compute_action(self, action, raw_obs):
         '''
@@ -27,16 +29,34 @@ class BuildMarinesActuator:
         :param raw_obs: Observations from pysc2
         :returns: The raw action to return to environment
         '''
-        selected = raw_obs.observation.feature_screen.selected
-
-        if action == BuildMarinesAction.BUILD_DEPOT:
-            # FIXME
-            return self._select_scv(raw_obs)
+        assert self.in_progress is None or action is None or action == self.in_progress
 
         if action == BuildMarinesAction.NO_OP:
             return actions.FUNCTIONS.no_op()
+        
+        if action == BuildMarinesAction.BUILD_DEPOT:
+            return self._build_depot(raw_obs)
 
         return actions.FUNCTIONS.no_op()
+
+    def _build_depot(self, raw_obs):
+        self.in_progress = BuildMarinesAction.BUILD_DEPOT
+
+        selected = raw_obs.observation.single_select
+        # NOTE Assumes selected has 1 element
+        if selected[0].unit_type != units.Terran.SCV.value:
+            # FIXME Cannot succesfully select SCVs
+            print('SELECTING')
+            return self._select_scv(raw_obs)
+        
+        if not self.depot_placed:
+            print('PLACING')
+            self.depot_placed = True
+            return self._place_depot(raw_obs)
+        
+        self.in_progress = None
+        self.depot_placed = False
+        return self._queue_gather(raw_obs)
 
     @staticmethod
     def _select_scv(raw_obs):
@@ -45,16 +65,35 @@ class BuildMarinesActuator:
         '''
         unit_types = raw_obs.observation.feature_screen.unit_type
         scv_locations = np.transpose(np.nonzero(unit_types == units.Terran.SCV.value))
-        # TODO which way is left? NOT THIS WAY
-        # TODO currently selecting top
-        select_point = np.flip(scv_locations[0], 0)
+        # Switch x-coordinate to be 1st
+        scv_locations = np.flip(scv_locations, 1)
+        select_point = np.amin(scv_locations, axis=0)
         return actions.FUNCTIONS.select_point('select', select_point)
 
     @staticmethod
-    def _build_depot(raw_obs):
+    def _place_depot(raw_obs):
         '''
         Assuming an SCV is selected, builds a supply depot
         '''
+        # Check if enough minerals
+        DEPOT_COST = 100
+        minerals = raw_obs.observation.player.minerals
+        if minerals < DEPOT_COST:
+            return actions.FUNCTIONS.no_op()
         # TODO compute free location to build
-        build_location = 0
+        build_location = (0, 5)
+        # TODO decide correct screen dimensions by observing Depot height and width, possibly 640 x 480
+        print(raw_obs.observation.feature_screen.unit_density_aa)
         return actions.FUNCTIONS.Build_SupplyDepot_screen('now', build_location)
+
+    @staticmethod
+    def _queue_gather(raw_obs):
+        '''
+        Assuming an SCV is selected, queues it back to collecting minerals
+        '''
+        unit_types = raw_obs.observation.feature_screen.unit_type
+        mineral_locations = np.transpose(np.nonzero(unit_types == units.Neutral.MineralField.value))
+        mineral_location = np.flip(mineral_locations[0], 0)
+        print(mineral_locations)
+        print(mineral_location)
+        return actions.FUNCTIONS.Harvest_Gather_screen('queued', mineral_location)
--- experiments/mcts_ac/custom_env.py
+++ experiments/mcts_ac/custom_env.py
@@ -5,7 +5,9 @@ from enum import Enum
 from abstract_core import CustomEnvironment
 
 class BuildMarinesEnvironment(CustomEnvironment):
+    # TODO decide correct screen dimensions by observing Depot height and width, possibly 640 x 480
     SCREEN_SIZE = 84
+    MINIMAP_SIZE = 1
     MAP = 'BuildMarines'
 
     def __init__(self, render=False, step_multiplier=None):
@@ -24,7 +26,7 @@ class BuildMarinesEnvironment(CustomEnvironment):
             map_name=self.MAP,
             agent_interface_format=features.AgentInterfaceFormat(
                 feature_dimensions=features.Dimensions(
-                    screen=self.SCREEN_SIZE, minimap=self.SCREEN_SIZE),
+                    screen=self.SCREEN_SIZE, minimap=self.MINIMAP_SIZE),
                 use_feature_units=True
             ),
             step_mul=step_multiplier,
@@ -44,7 +46,7 @@ class BuildMarinesEnvironment(CustomEnvironment):
         self._actuator.reset()
         self._terminal = False
 
-        self._run_to_next()
+        self._reset_env()
         self._terminal = self._curr_frame.last()
         agent_obs = self._curr_frame
 
@@ -67,16 +69,16 @@ class BuildMarinesEnvironment(CustomEnvironment):
         agent_obs = self._curr_frame
         return [agent_obs], [self._curr_frame.reward], self._curr_frame.last(), [None]
 
-    def _run_to_next(self, start_action=None):
-        if start_action is None:
-            self._reset_env()
-            start_action = BuildMarinesAction.NO_OP
-
-        if self._curr_frame.last():
-            return
-
+    def _run_to_next(self, start_action):
         raw_action = self._actuator.compute_action(start_action, self._curr_frame)
         self._step_env(raw_action)
+        
+        while self._actuator.in_progress is not None:
+            if self._curr_frame.last():
+                return
+
+            raw_action = self._actuator.compute_action(start_action, self._curr_frame)
+            self._step_env(raw_action)
     
     def _reset_env(self):
         self._prev_frame = self._curr_frame
