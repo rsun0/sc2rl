bfbb8d1a55ed19cc517bf75e899fa1cd410dc246
Ray Sun <raysun2@illinois.edu>
2020-02-09 11:54:59 -0600

    Wrote BUILD_BARRACKS

--- experiments/mcts_ac/action_interface.py
+++ experiments/mcts_ac/action_interface.py
@@ -14,8 +14,12 @@ class BuildMarinesAction(Enum):
 
 class BuildMarinesActuator:
     DEPOT_COST = 100
-    DEPOT_LOCATIONS = [(30 + 7*i, 5 + 7*j) for j in range(3) for i in range(8)]
-    MAX_DEPOTS = 23
+    BARRACKS_COST = 100
+    DEPOT_LOCATIONS = [(20 + 7*i, 5 + 7*j) for j in range(2) for i in range(9)]
+    BARRACKS_LOCATIONS = ([(20 + 10*i, 60) for i in range(6)] + [(83, 60)]
+        + [(52 + 10*i, 40) for i in range(4)])
+    MAX_DEPOTS = 2
+    MAX_BARRACKS = 11
 
     def __init__(self):
         self.reset()
@@ -25,6 +29,7 @@ class BuildMarinesActuator:
         self.progress_stage = 0
         self.selecting = False
         self.num_depots = 0
+        self.num_barracks = 0
 
     def compute_action(self, action, raw_obs):
         '''
@@ -39,42 +44,20 @@ class BuildMarinesActuator:
             # Continue SCV selection
             return self._select_scv(raw_obs)
 
+        if action == BuildMarinesAction.MAKE_SCV:
+            # TODO 22 SCVs is optimal, 23rd is useless
+            pass
         if action == BuildMarinesAction.NO_OP:
             return actions.FUNCTIONS.no_op()
         if action == BuildMarinesAction.BUILD_DEPOT:
             return self._build_depot(raw_obs)
+        if action == BuildMarinesAction.BUILD_BARRACKS:
+            return self._build_barracks(raw_obs)
+        if action == BuildMarinesAction.KILL_MARINE:
+            # TODO KILL_MARINE should be queued action, not now
+            pass
 
         return actions.FUNCTIONS.no_op()
-
-    def _build_depot(self, raw_obs):
-        self.in_progress = BuildMarinesAction.BUILD_DEPOT
-        
-        if self.progress_stage == 0:
-            if self.num_depots >= self.MAX_DEPOTS:
-                print('Reached maximum number of Supply Depots')
-                return self._conclude_sequence(raw_obs)
-            minerals = raw_obs.observation.player.minerals
-            if minerals < self.DEPOT_COST:
-                print('Not enough minerals for Supply Depot')
-                return self._conclude_sequence(raw_obs)
-            self.progress_stage += 1
-            return self._select_scv(raw_obs)
-
-        if self.progress_stage == 1:
-            available = raw_obs.observation.available_actions
-            if actions.FUNCTIONS.Build_SupplyDepot_screen.id not in available:
-                print('Cannot build depot')
-                return self._conclude_sequence(raw_obs)
-            self.progress_stage += 1
-            return self._place_depot(raw_obs, self.num_depots)
-
-        if self.progress_stage == 2:
-            self.progress_stage += 1
-            return self._queue_gather(raw_obs)
-
-        assert self.progress_stage == 3 
-        self.num_depots += 1
-        return self._conclude_sequence(raw_obs)
 
     def _conclude_sequence(self, raw_obs):
         '''
@@ -99,6 +82,69 @@ class BuildMarinesActuator:
             self.selecting = False
             return actions.FUNCTIONS.select_unit('select', 0)
 
+    def _build_depot(self, raw_obs):
+        self.in_progress = BuildMarinesAction.BUILD_DEPOT
+        
+        if self.progress_stage == 0:
+            if self.num_depots >= self.MAX_DEPOTS:
+                print('Reached maximum number of Supply Depots')
+                return self._conclude_sequence(raw_obs)
+            if raw_obs.observation.player.minerals < self.DEPOT_COST:
+                print('Not enough minerals for Supply Depot')
+                return self._conclude_sequence(raw_obs)
+            self.progress_stage += 1
+            return self._select_scv(raw_obs)
+
+        if self.progress_stage == 1:
+            available = raw_obs.observation.available_actions
+            if actions.FUNCTIONS.Build_SupplyDepot_screen.id not in available:
+                print('Build_SupplyDepot_screen not in available_actions')
+                return self._conclude_sequence(raw_obs)
+            self.progress_stage += 1
+            return self._place_depot(raw_obs, self.num_depots)
+
+        if self.progress_stage == 2:
+            self.progress_stage += 1
+            return self._queue_gather(raw_obs)
+
+        assert self.progress_stage == 3 
+        self.num_depots += 1
+        return self._conclude_sequence(raw_obs)
+
+    def _build_barracks(self, raw_obs):
+        MIN_SUPPLY = 23
+
+        self.in_progress = BuildMarinesAction.BUILD_BARRACKS
+        
+        if self.progress_stage == 0:
+            if raw_obs.observation.player.food_cap < MIN_SUPPLY:
+                print('Cannot build Barracks without Supply Depot')
+                return self._conclude_sequence(raw_obs)
+            if self.num_barracks >= self.MAX_BARRACKS:
+                print('Reached maximum number of Barracks')
+                return self._conclude_sequence(raw_obs)
+            if raw_obs.observation.player.minerals < self.BARRACKS_COST:
+                print('Not enough minerals for Barracks')
+                return self._conclude_sequence(raw_obs)
+            self.progress_stage += 1
+            return self._select_scv(raw_obs)
+
+        if self.progress_stage == 1:
+            available = raw_obs.observation.available_actions
+            if actions.FUNCTIONS.Build_Barracks_screen.id not in available:
+                print('Build_Barracks_screen not in available_actions')
+                return self._conclude_sequence(raw_obs)
+            self.progress_stage += 1
+            return self._place_barracks(raw_obs, self.num_barracks)
+
+        if self.progress_stage == 2:
+            self.progress_stage += 1
+            return self._queue_gather(raw_obs)
+
+        assert self.progress_stage == 3 
+        self.num_barracks += 1
+        return self._conclude_sequence(raw_obs)
+
     @staticmethod
     def _select_cc(raw_obs):
         '''
@@ -115,17 +161,25 @@ class BuildMarinesActuator:
         return actions.FUNCTIONS.select_point('select', cc_location)
 
     @staticmethod
-    def _place_depot(raw_obs, location_index):
-        '''
-        Assuming an SCV is selected, builds a supply depot
-        '''
-        build_location = BuildMarinesActuator.DEPOT_LOCATIONS[location_index]
-        return actions.FUNCTIONS.Build_SupplyDepot_screen('now', build_location)
-
-    @staticmethod
     def _queue_gather(raw_obs):
         '''
         Assuming an SCV is selected, queues it back to collecting minerals
         '''
         MINERAL_LOCATION = (14, 14)
         return actions.FUNCTIONS.Harvest_Gather_screen('queued', MINERAL_LOCATION)
+
+    @staticmethod
+    def _place_depot(raw_obs, location_index):
+        '''
+        Assuming an SCV is selected, builds a supply depot
+        '''
+        build_location = BuildMarinesActuator.DEPOT_LOCATIONS[location_index]
+        return actions.FUNCTIONS.Build_SupplyDepot_screen('now', build_location)
+    
+    @staticmethod
+    def _place_barracks(raw_obs, location_index):
+        '''
+        Assuming an SCV is selected, builds a barracks
+        '''
+        build_location = BuildMarinesActuator.BARRACKS_LOCATIONS[location_index]
+        return actions.FUNCTIONS.Build_Barracks_screen('now', build_location)
